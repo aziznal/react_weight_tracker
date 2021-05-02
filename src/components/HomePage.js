091ba7ab543b485f7171a35313df99826a9ada89
aziznal <aziznal.dev@gmail.com>
2021-05-03 02:22:01 +0300

    Add button to trigger 'add weight' menu

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import {useState} from 'react';
 
 import BackendService from '../BackendService';
 
@@ -9,48 +10,67 @@ import SummarySection from './SummarySection';
 
 const HomePage = ({ weight, setWeight, currentUnit, setUnit, measureDate, setMeasureDate, measureTime, setMeasureTime }) => {
 
+    let [showAddWeight, setShowAddWeight] = useState(false);
+
     const onSubmit = async (entry) => {
         await BackendService.addEntry(entry);
     }
 
     return (
         <div className="body-wrapper">
-            {/* ### */}
-            <MetricImperialSwitch  currentUnit={currentUnit} updateUnit={(newUnit) => setUnit(newUnit)} />
-
-            <div className="separator"></div>
-
-            {/* ### */}
-            <WeightForm
-
-                weight={weight}
-                updateWeight={setWeight}
-                updateUnit={setUnit}
-                currentUnit={currentUnit}
-
-                updateDate={setMeasureDate}
-                updateTime={setMeasureTime}
-
-            />
-
-            <div className="separator"></div>
 
 
-            < SummarySection
+            <div className="homepage-container" >
 
-                currentUnit={currentUnit}
-                weight={weight}
-                date={measureDate}
-                time={measureTime}
-                onEntrySubmit={onSubmit}
+                <button onClick={ () => setShowAddWeight(!showAddWeight) }>
+                    Me Cliquez!
+                </button>
 
-            />
+                <div className="separator"></div>
 
-            <div className="separator"></div>
+                <div className="row">
+                    <Link to={'/about'}>About</Link>
+                </div>
 
-            <div className="row">
-                <Link to={'/about'}>About</Link>
+
             </div>
+
+            {
+                showAddWeight &&
+                <div className="column weight-add-menu slide-from-left">
+                    {/* ### */}
+                    <MetricImperialSwitch currentUnit={currentUnit} updateUnit={(newUnit) => setUnit(newUnit)} />
+
+                    <div className="separator"></div>
+
+                    {/* ### */}
+                    <WeightForm
+
+                        weight={weight}
+                        updateWeight={setWeight}
+                        updateUnit={setUnit}
+                        currentUnit={currentUnit}
+
+                        updateDate={setMeasureDate}
+                        updateTime={setMeasureTime}
+
+                    />
+
+                    <div className="separator"></div>
+
+
+                    < SummarySection
+
+                        currentUnit={currentUnit}
+                        weight={weight}
+                        date={measureDate}
+                        time={measureTime}
+                        onEntrySubmit={onSubmit}
+
+                    />
+                </div>
+
+            }
 
         </div>
     )
