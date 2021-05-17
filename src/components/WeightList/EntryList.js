f3a3b477ad7d6bb74cc82a84be0f6447ec964771
aziznal <aziznal.dev@gmail.com>
2021-05-17 07:34:21 +0300

    Extracts new component from WeightList.js

---
+++ src/components/WeightList/EntryList.js
@@ -0,0 +1,102 @@
+import React from 'react'
+
+import PropTypes from 'prop-types';
+
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+
+import Entry from './Entry/Entry';
+
+
+const propTypes = {
+
+	entries: PropTypes.array.isRequired,
+
+	onDeleteEntry: PropTypes.func.isRequired,
+
+	askBeforeDelete: PropTypes.bool.isRequired,
+
+}
+
+// FIXME: Entry List is re-rendering on every small change
+
+const EntryList = React.memo(({ entries, onDeleteEntry, askBeforeDelete }) => {
+
+	console.log("yo" + (Date.now()%100))
+
+	const onDeleteEntryWrapper = (entry_id) => {
+
+		if (askBeforeDelete) {
+
+			confirmAlert({
+				customUI: ({ onClose }) => {
+					return (
+
+						<div className="confirm-delete-dialog">
+
+							<h1>Confirm Delete</h1>
+							<p>Are you sure you want to delete entry #{entry_id}?</p>
+
+							<div className="row">
+
+								<button className="btn" onClick={onClose} style={{ backgroundColor: "red" }}>
+									Cancel
+								</button>
+
+								<button className="btn" onClick={() => { onDeleteEntry(entry_id); onClose(); }}>
+									Yes
+								</button>
+
+							</div>
+
+						</div>
+
+					)
+				}
+			})
+
+		} else {
+
+			onDeleteEntry(entry_id)
+
+		}
+
+
+
+	}
+
+	const createEntriesList = () => {
+
+		const formattedEntryList = [];
+
+		for (let entry of entries) {
+
+			formattedEntryList.push(
+				<Entry key={entry.id} entryData={entry} onDeleteEntry={onDeleteEntryWrapper} />
+			)
+
+		}
+
+		return formattedEntryList;
+
+	}
+
+
+	return (
+		<section className="weight-list" id="entry-section">
+
+		<ul>
+
+			{
+				createEntriesList()
+			}
+
+		</ul>
+
+	</section>
+	)
+});
+
+EntryList.propTypes = propTypes;
+
+export default EntryList
